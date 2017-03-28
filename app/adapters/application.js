062911b2ce5eaf7b376f46672489950261d43e50
Michaël Dierick <michael@dierick.io>
2017-03-28 21:14:19 +0200

    Tackle CORS by proxy instead of in application adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  // namespace: 'api/1',
-  host: 'http://localhost/'
+    // namespace: 'api/1',
+    // host: 'http://localhost/'
 });
---
+++ app/serializers/fietsroute.js
@@ -0,0 +1,26 @@
+import DS from 'ember-data';
+
+export default DS.JSONAPISerializer.extend({
+  /* Method & code as described at https://emberigniter.com/custom-relationship-links-json-api/
+  */
+  normalizeFindAllResponse(store, type, payload) {
+    payload.data = payload.data.map(this.addLinks);
+    return payload;
+  },
+
+  normalizeFindRecordResponse(store, type, payload) {
+    payload.data = this.addLinks(payload.data);
+    return payload;
+  },
+
+  addLinks(fietsroute) {
+    // fietsroute.type = 'fietsroute'; //already there, no?
+    // delete fietsroute.relationships.segments.data;
+    fietsroute.relationships.segments = {}
+    fietsroute.relationships.segments.links = {
+      related: `map2osm/fietsroutes/${fietsroute.id}/segments`
+    };
+    return fietsroute;
+  }
+
+});
