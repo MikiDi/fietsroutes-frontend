da9dc0b5bf2f53516ec1d5648869671adce47fdc
Michaël Dierick <michael@dierick.io>
2017-06-27 09:55:12 +0200

    Presentatie

--- app/routes/fietsroute.js
+++ app/routes/fietsroute.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.get('store').findRecord('fietsroute', params.fietsroute_id);
-  }
+  },
+
+  onEachFeature(feature, layer) {
+    feature.bindTooltip("my tooltip");
+    console.log("bound tooltip");
+    // layer.bindPopup(popupContent);
+}
+
 });
